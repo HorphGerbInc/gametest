f0c18751bf307eaa069602b41a8b85bf7e3eff21
Jeffrey Robinson <deathly809@gmail.com>
2017-10-15 16:31:18 -0700

    fixed resource tests

--- test/Resource/Resource.Test.cpp
+++ test/Resource/Resource.Test.cpp
@@ -1,23 +1,5 @@
 // Main entry point for common tests.
 
-#define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_MAIN  
 #include <catch.hpp>
 
-#include <Platform/Window.hpp>
-#include <Render/OpenGL.hpp>
-
-int main(int argc, char *argv[]) {
-  int height = 800;
-  int width = height * 16 / 10;
-  jerobins::platform::Window *ptrWindow =
-      jerobins::platform::Window::Create("test", height, width);
-
-  if (!gladLoadGL()) {
-    std::cout << "could not load opengl" << std::endl;
-    exit(-1);
-  }
-
-  int result = Catch::Session().run(argc, argv);
-
-  return (result < 0xff ? result : 0xff);
-}
